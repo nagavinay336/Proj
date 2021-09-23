5df6676c09aee3f1fa1829d1c4c39adce2126598
NagaVinayEM <nagavinay336@gmail.com>
2021-09-23 13:24:52 +0530

    Animals

---
+++ Animals/src/org/animals/Deer.java
@@ -0,0 +1,38 @@
+package org.animals;
+
+public class Deer {
+	private String color;
+	private int weight;
+	private int age;
+	private boolean veg,climb;
+	
+	public boolean canClimb() {
+		return climb;
+	}
+
+	public boolean isVeg() {
+		return veg;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Deer(String color,int weight,int age,boolean veg,boolean climb){
+		this.color=color;
+		this.weight=weight;
+		this.age=age;
+		this.climb=climb;
+		this.veg=veg;
+	}
+	
+}
---
+++ Animals/src/org/animals/Tiger.java
@@ -0,0 +1,37 @@
+package org.animals;
+
+public class Tiger {
+	private String color;
+	private int weight;
+	private int age;
+	private boolean veg,climb;
+	
+	public boolean canClimb() {
+		return climb;
+	}
+
+	public boolean isVeg() {
+		return veg;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Tiger(String color,int weight,int age,boolean veg,boolean climb){
+		this.color=color;
+		this.weight=weight;
+		this.age=age;
+		this.climb=climb;
+		this.veg=veg;
+	}
+}
